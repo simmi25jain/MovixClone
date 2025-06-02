06538e5ca9ce8a4b6d2e0ea30de5f722cff19291
simmi25jain <simmi5jain4@gmail.com>
2025-06-02 14:36:29 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaSearch } from "react-icons/fa";
+import './Navbar.css'
+import Logo from '../assets/Logo.png'
+
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <div>
+            <div className="navbar">
+                <div className="moviesite"><Link to="/"><img src={Logo} alt="Logo" className="logo" /></Link></div>
+                <div className="right">
+                    <Link to="/Movies">MOVIES</Link>
+                    <Link to="/Tvshows">TVSHOWS</Link>
+                    <div><FaSearch /></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar

5f6eb037148eb39df07fc71bb7d868b56084cd85
simmi25jain <simmi5jain4@gmail.com>
2025-06-09 15:34:31 +0530

    button changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,29 +5,50 @@ const baseImageURL = "https://image.tmdb.org/t/p/original";
 
 function Home({ heading, btn1, btn2, urls }) {
   const [allMovieData, setAllMovieData] = useState([]);
+  const [showData, setShowData] = useState(urls[0]);
+
+  // useEffect(() => {
+  //   async function fetchTrendingMoviesDay() {
+  //     try {
+  //       const responses = await Promise.all(urls.map((url) => fetch(url)));
+  //       const results = await Promise.all(responses.map((res) => res.json()));
+  //       const data = results.flatMap((res) => res.results || []);
+  //       setAllMovieData(data);
+  //     } catch (err) {
+  //       console.error("Error fetching trending movies:", err);
+  //     }
+  //   }
+
+  //   fetchTrendingMoviesDay();
+  // }, [showData]);
 
   useEffect(() => {
-    async function fetchTrendingMoviesDay() {
+    async function fetchTrendingMovies() {
       try {
-        const responses = await Promise.all(urls.map((url) => fetch(url)));
-        const results = await Promise.all(responses.map((res) => res.json()));
-        const data = results.flatMap((res) => res.results || []);
-        setAllMovieData(data);
+        const res = await fetch(showData);
+        const json = await res.json();
+        setAllMovieData(json.results || []);
       } catch (err) {
-        console.error("Error fetching trending movies:", err);
+        console.error("Error fetching movies:", err);
       }
     }
+    fetchTrendingMovies();
+  }, [showData]);
 
-    fetchTrendingMoviesDay();
-  }, [urls]);
+  function trimContent(content) {
+    if (content.length > 28) {
+      return content.slice(0, 16) + "...";
+    }
+    return content;
+  }
 
   return (
     <div className="container">
       <div className="heading-btn">
         <h1 className="heading">{heading}</h1>
         <div className="homeBtns">
-          <button className="btn1">{btn1}</button>
-          <button className="btn2">{btn2}</button>
+          <button className="btn1" onClick={() => setShowData(urls[0])}>{btn1}</button>
+          <button className="btn2" onClick={() => setShowData(urls[1])}>{btn2}</button>
         </div>
       </div>
 
@@ -41,8 +62,12 @@ function Home({ heading, btn1, btn2, urls }) {
                 alt={movie.title || movie.name}
               />
             </div>
-            <div className="movieTitle">{movie.original_title || movie.name}</div>
-            <div className="releaseDate">{movie.release_date || movie.first_air_date}</div>
+            <div className="movieTitle">{trimContent(movie.original_title || movie.name)}</div>
+            <div className="releaseDate">{new Date(movie.release_date || movie.first_air_date).toLocaleDateString("en-US", {
+              day: "numeric",
+              month: "long",
+              year: "numeric",
+            })}</div>
           </li>
         ))}
       </ul>
